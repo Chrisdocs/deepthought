e02697ab54a9ac2c84dc9301723206eaf0aea363
Chrisdocs <chrisa.wolfe@gmail.com>
2021-08-11 14:33:47 -0500

    mutation requests for me, addthought, addreaction, addfriend

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -3,6 +3,7 @@ const { gql } = require('apollo-server-express');
 
 //create our typeDefs
 const typeDefs = gql`
+
     type Thought {
         _id: ID
         thoughtText: String
@@ -10,13 +11,14 @@ const typeDefs = gql`
         username: String
         reactionCount: Int
         reactions: [Reaction]
-    }
+    },
+
     type Reaction {
         _id: ID
         reactionBody: String
         createdAt: String
         username: String
-    }
+    },
 
     type User {
         _id: ID
@@ -25,12 +27,27 @@ const typeDefs = gql`
         friendCount: Int
         thoughts: [Thought]
         friends: [User]
-    }
+    },
+
+    type Auth {
+        token: ID!
+        user: User
+    },
+
     type Query {
+        me: User
         users: [User]
         user(username: String!): User
         thoughts(username: String): [Thought]
         thought(_id: ID!): Thought
+    },
+
+    type Mutation {
+        login(email: String!, password: String!): Auth
+        addUser(username: String!, email: String!, password: String!): Auth
+        addThought(thoughtText: String!): Thought
+        addReaction(thoughtId: ID!, reactionBody: String!): Thought
+        addFriend(friendId: ID!): User
     }
 `;
 
