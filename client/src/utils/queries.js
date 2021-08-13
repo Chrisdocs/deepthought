947b52c992ea31ea1f700f2dc61717b7d45c8de3
Chrisdocs <chrisa.wolfe@gmail.com>
2021-08-12 19:45:50 -0500

    set for merge

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -57,25 +57,30 @@ export const QUERY_USER = gql`
 `;
 
 export const QUERY_ME = gql`
-    {
-        me {
-            _id
-            username
-            email
-            friendCount
-            thoughts {
-                _id
-                thoughtText
-                createdAt
-                reactionBody
-                username
-            }
+  {
+    me {
+      _id
+      username
+      email
+      friendCount
+      thoughts {
+        _id
+        thoughtText
+        createdAt
+        reactionCount
+        reactions {
+          _id
+          createdAt
+          reactionBody
+          username
         }
-        friends {
-            _id
-            username
-        }
+      }
+      friends {
+        _id
+        username
+      }
     }
+  }
 `;
 
 export const QUERY_ME_BASIC = gql`
